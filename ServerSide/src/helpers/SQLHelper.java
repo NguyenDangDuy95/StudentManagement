94ebaf014ae3e44f3c00c73a68fe89114eb340d3
a8thunder <minhtritruong0209@gmail.com>
2017-08-22 17:11:41 +0700

    22/08/2017

--- ServerSide/src/helpers/SQLHelper.java
+++ ServerSide/src/helpers/SQLHelper.java
@@ -3,10 +3,10 @@ package helpers;
 public class SQLHelper {
 
     public static final String JDBC_DRIVER = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
-    public static final String SERVER_NAME = "DESKTOP-U2T7NV9";
-    public static final String DB_NAME = "SCHOOLMANAGEMENT";
+    public static final String SERVER_NAME = "TMT\\SQLEXPRESS:1433";
+    public static final String DB_NAME = "ManagerAptech";
     public static final String DB_URL = "jdbc:sqlserver://" + SERVER_NAME + "; databaseName=" + DB_NAME;
-    public static final String PASS = "123456789";
+    public static final String PASS = "123";
     public static final String STUDENT_TABLE = "StudentList";
     public static final String STUDENT_INFO_TABLE = "StudentInfomation";
     public static final String EMPLOYEE_TABLE = "EmployeeList";
