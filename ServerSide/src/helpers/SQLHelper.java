bd2e5d3b87a3c10d8f680d2b99a4387e89056a3d
Duy Nguyen <ndangduy95@gmail.com>
2017-09-01 23:00:37 +0700

    clean code

--- ServerSide/src/helpers/SQLHelper.java
+++ ServerSide/src/helpers/SQLHelper.java
@@ -6,21 +6,42 @@ public class SQLHelper {
     public static final String SERVER_NAME = "DESKTOP-U2T7NV9";
     public static final String DB_NAME = "ManagerAptech";
     public static final String DB_URL = "jdbc:sqlserver://" + SERVER_NAME + "; databaseName=" + DB_NAME;
+    
+    public static final String USER = "sa";
     public static final String PASS = "123456789";
+    
     public static final String STUDENT_TABLE = "StudentList";
-    public static final String STUDENT_INFO_TABLE = "StudentInfomation";
+    public static final String STUDENT_INFO_TABLE = "InformationStudent";
+    
+    public static final String COURSE_TABLE = "Course";
+    
+    public static final String SUBJECT_LIST_TABLE = "SubjectList";
+    
+    public static final String SCORE_TABLE = "Score";
+    
+    public static final String ATTDANCE_TABLE = "Attendance"; 
+    
+    public static final String BATCH_LIST_TABLE ="BatchList";
+    
+    public static final String SUBJECT_TEACHER_TABLE = "SubjectTeacher";
+    
+    public static final String POSITION_TABLE = "Position";
+    
+    public static final String SALARY_TABLE = "Salary";
+    
     public static final String EMPLOYEE_TABLE = "EmployeeList";
     public static final String EMPLOYEE_INFO_TABLE = "InfomationEmployee";
-    public static final String COURSE_TABLE = "Course";
-    public static final String USER = "sa";
-    public static final String GetAllUser = "SELECT * FROM LoginAccount;";
-  
     
-    public static final String GetAllEmployee = "SELECT * FROM ";
 
+    public static final String GetAllEmployee = "SELECT * FROM ";
+    public static final String GetCourseList = "SELECT * FROM "+ COURSE_TABLE;
+    public static final String GetAllUser = "SELECT * FROM LoginAccount;";
     public static final String GetStudentList = "SELECT * FROM "+STUDENT_TABLE;
     public static final String GetEmployeeList = "SELECT * FROM "+EMPLOYEE_TABLE;
-    public static final String GetCourseList = "SELECT * FROM "+ COURSE_TABLE;
+  
+    
+    
+    
     public static String getBatchListByCourseID(String id){
         return String.format("SELECT * FROM BatchList WHERE CourseID = \'%s\'", id);
     }
@@ -29,57 +50,13 @@ public class SQLHelper {
         return String.format("SELECT * FROM SubjectList WHERE CourseID = \'%s\'", id);
     }
     
-    
-    
-    public static String getStudentInfoByID(String id){
-        return "SELECT * FROM " + STUDENT_INFO_TABLE + " WHERE StudentID = \'"+id+"\';";
-    }
-    public static final String SUBJECT_TEACHER_TABLE = "SubjectTeacher";
-    public static final String POSITION_TABLE = "Position";
-    public static final String SALARY_TABLE = "Salary";
-    
-//---------------------------------------------------LOGIN--------------------------------------------------------------
-    public static final String GetAllUser = "SELECT * FROM LoginAccount;";
-    
-    public static String createSelectSQL(String tableName, String item) {
-        return String.format("SELECT %s FROM %s;", item, tableName);
-    }
-    
-    public static String getElementByID(String id, String role) {
-        if (role.equals("student")) {
-            return "SELECT * FROM " + STUDENT_TABLE
-                    + " LEFT JOIN " + STUDENT_INFO_TABLE
-                    + " ON " + STUDENT_TABLE + ".StudentID = " 
-                    + STUDENT_INFO_TABLE + ".StudentID WHERE StudentID = \'" + id + "\'";
-        } else if (role.equals("employee")) {
-            return String.format(" SELECT * "
-            + "FROM EmployeeList "
-            + "LEFT JOIN InformationEmployee ON InformationEmployee.EmployeeID = EmployeeList.EmployeeID "
-            + "LEFT JOIN Position ON Position.PositionID = EmployeeList.PositonID "
-            + "WHERE EmployeeList.EmployeeID = '%s' AND Position.PositionName = '%s';", id,"employee");
-        } else {
-            return String.format(" SELECT * "
-            + "FROM EmployeeList "
-            + "LEFT JOIN InformationEmployee ON InformationEmployee.EmployeeID = EmployeeList.EmployeeID "
-            + "LEFT JOIN Position ON Position.PositionID = EmployeeList.PositonID "
-            + "WHERE EmployeeList.EmployeeID = '%s' AND Position.PositionName = '%s';", id,"admin");
-        }
-    } 
-
- //---------------------------------------------------STUDENT--------------------------------------------------------------- 
-    
-    public static final String GetStudentList = "SELECT * FROM "+STUDENT_TABLE;
- 
     public static String getStudentInfoByID(String id){
         return "SELECT * FROM " + STUDENT_INFO_TABLE + " WHERE StudentID = \'"+id+"\';";
     }
     
- // CourseID : ACCP i15 , ACCP i17 
-    public static String GetSubjectByCourseID(String courseID){
-        return "SELECT CourseID, SubjectID, SubjectName, Hours ,Semester" +
-                "FROM"+ SUBJECT_LIST_TABLE +
-                "WHERE CourseID = \'"+courseID+"\';";
-    }   
+    public static String GetEmployeeByID(String id){
+        return null;
+    }
 
     public static String GetStudentByID(String id){
         return "SELECT StudentList.StudentID , FirstName ,LastName , Gender , Birthday , IDCard , Addresss, PlaceOfBirth, PhoneNumber, Email," +
@@ -88,6 +65,7 @@ public class SQLHelper {
             "LEFT JOIN "+ STUDENT_INFO_TABLE +"ON StudentList.StudentID = StudentInformation.StudentID" +
             "WHERE StudentList.StudentID = \'"+id+"\';";
     }
+    
     // hiển thị thông tin người dạy - môn đã và đang dạy :
     public static final String GetscheduleByBatchID(String id){
         return "SELECT DISTINCT FirstName, LastName, SubjectTeacher.CourseID, BatchID, Semester, SubjectName, SubjectTeacher.StartDate, SubjectTeacher.EndDate" +
@@ -97,17 +75,4 @@ public class SQLHelper {
             "WHERE BatchID = \'"+id+"\'" +
             "ORDER BY "+SUBJECT_TEACHER_TABLE+".StartDate ASC;";
     }
-    
- //---------------------------------------------------EMPLOYEE--------------------------------------------------------------  
-    
-    public static final String GetEmployeeList = "SELECT * FROM "+EMPLOYEE_TABLE;
-
-
-    
-    public static String GetEmployeeByID(String id){
-        return null;
-    }
-    
-    
-
 }
